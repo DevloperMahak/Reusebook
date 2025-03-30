7b6e38263f3b94aea7db18004da6705aa96bbc7b
DeveloperMahak <mahakguptaji2005@gmail.com>
2025-03-30 14:08:02 +0530

    functionality added

--- lib/favourites.dart
+++ lib/favourites.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:get/get.dart';
 import 'package:reusebook/sell.dart';
 import 'package:reusebook/shopkeeper.dart';
 
 import 'categories.dart';
+import 'controllers/favorites_controller.dart';
 import 'home.dart';
 import 'orders.dart';
 
@@ -12,9 +14,17 @@ class favouritePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final screenwidth=MediaQuery.of(context).size.width;
+    final FavoritesController favoritesController = Get.put(FavoritesController());
+
     return Scaffold(
       appBar: AppBar(
-        title: Text("Favourites",style: TextStyle(fontSize:25,color: Colors.black),),
+        title: Text('favourites'.tr,style: TextStyle(fontSize:25,color: Colors.black),),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.black), // Back arrow icon
+          onPressed: () {
+            Navigator.push(context,MaterialPageRoute(builder: (context)=>homePage()));
+          },
+        ),
         flexibleSpace:Container(
           decoration: BoxDecoration(
               gradient: LinearGradient(colors: [
@@ -28,24 +38,70 @@ class favouritePage extends StatelessWidget {
       body: Stack(
           children: [
             Center(
-                child: Container(
-                  margin: EdgeInsets.only(top: 180,left: 20,right: 20),
-                  child:Column(
-                    children: [
-                      Text("Your favourited products(0 item)",textAlign:TextAlign.center,style: TextStyle(fontSize:18,color: Colors.grey,fontWeight: FontWeight.w500),),
-                      Container(
-                          margin: EdgeInsets.only(bottom: 20,top: 20),
-                          height:100,
-                          width: 100,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(50),
-                            color: Color(0xffFFD77F),
+                child: Obx(() {
+                  if (favoritesController.favoriteBooks.isEmpty) {
+                    return Center(
+                      child: Container(
+                        margin: EdgeInsets.only(top: 180, left: 20, right: 20),
+                        child: Column(
+                          children: [
+                            Text("Your favourited products (0 item)",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    color: Colors.grey,
+                                    fontWeight: FontWeight.w500)),
+                            Container(
+                              margin: EdgeInsets.only(bottom: 20, top: 20),
+                              height: 100,
+                              width: 100,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(50),
+                                color: Color(0xffFFD77F),
+                              ),
+                              child: Icon(Icons.favorite_sharp,
+                                  size: 50, color: Color(0xff3D4652)),
+                            ),
+                            const Text("Wishlist is empty!",
+                                style: TextStyle(
+                                    fontSize: 22,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.w600)),
+                            Text(
+                                "Save products you like in wishlist. You can review and easily add them to cart anytime later.",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.w400)),
+                          ],
+                        ),
+                      ),
+                    );
+                  } else {
+                    return ListView.builder(
+                      padding: EdgeInsets.only(top: 20, bottom: 90),
+                      itemCount: favoritesController.favoriteBooks.length,
+                      itemBuilder: (context, index) {
+                        final book = favoritesController.favoriteBooks[index];
+                        return ListTile(
+                          leading: Icon(Icons.book, size: 40, color: Color(0xff3D4652)),
+                          //leading: Image.network(book.imageUrl, width: 50, height: 70), // adjust accordingly
+                          title: Text(book.bookName),
+                          subtitle: Text(book.author),
+                          trailing: IconButton(
+                            icon: Icon(Icons.delete, color: Color(0xff3D4652)),
+                            onPressed: () {
+                              favoritesController.removeFromFavorites(book);
+                              Get.snackbar("Removed", "${book.bookName} removed from favorites");
+                            },
                           ),
-                          child: Icon(Icons.favorite_sharp,size:50,color: Color(0xff3D4652))),
-                      const Text("Wishlist is empty!",style: TextStyle(fontSize:22,color: Colors.black,fontWeight: FontWeight.w600),),
-                      Text("Save products you like in wishlist. You can review and easily add them to cart anytime later.",textAlign:TextAlign.center,style: TextStyle(fontSize:18,color: Colors.black,fontWeight: FontWeight.w400),),
-                    ],
-                  ),)),
+                        );
+                      },
+                    );
+                  }
+                }),
+            ),
             Positioned(
                 bottom: 0,
                 child: Container(
@@ -80,7 +136,7 @@ class favouritePage extends StatelessWidget {
                                           width: 32,
                                           child: Image.asset(
                                             "assets/images/categories.png",),),),
-                                      Text("categories",
+                                      Text('categories'.tr,
                                           style: TextStyle(fontSize: 12, color: Color(
                                               0xff3D4652)))
                                     ]),),
@@ -98,7 +154,7 @@ class favouritePage extends StatelessWidget {
                                           width: 32,
                                           child: Image.asset(
                                             "assets/images/package.png",),)),
-                                      Text("orders",
+                                      Text('orders'.tr,
                                           style: TextStyle(fontSize: 12, color: Color(
                                               0xff3D4652)))
                                     ]),),
@@ -117,7 +173,7 @@ class favouritePage extends StatelessWidget {
                                               backgroundColor: Colors.white,
                                               child: Icon(Icons.home, size: 32,
                                                   color: Color(0xff3D4652)))),
-                                      Text("Home",
+                                      Text('home'.tr,
                                           style: TextStyle(fontSize: 12, color: Color(
                                               0xff3D4652)))
                                     ]),),
@@ -135,7 +191,7 @@ class favouritePage extends StatelessWidget {
                                           width: 32,
                                           child: Image.asset(
                                             "assets/images/merchant.png",),)),
-                                      Text("Shopkeepers",
+                                      Text('shopkeepers'.tr,
                                           style: TextStyle(fontSize: 12, color: Color(
                                               0xff3D4652)))
                                     ]),),
@@ -155,7 +211,7 @@ class favouritePage extends StatelessWidget {
                                             width: 32,
                                             child: Image.asset(
                                               "assets/images/sell.png",),)),
-                                      Text("Sell",
+                                      Text('sell'.tr,
                                           style: TextStyle(fontSize: 12, color: Color(
                                               0xff3D4652)))
                                     ]),),
