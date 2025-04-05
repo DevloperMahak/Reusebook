7341d0452e9d5ac9a714da961599ec7df0c60338
DeveloperMahak <mahakguptaji2005@gmail.com>
2025-04-05 23:16:56 +0530

    path changes

--- lib/url.dart
+++ lib/url.dart
@@ -1,4 +1,4 @@
-final url = 'http://192.168.236.59:5000/';
+final url = 'http://192.168.58.59:5000/';
 final registration = url + "registration";
 final login = url + "login";
 final postbookdetails = url + "postBook";
